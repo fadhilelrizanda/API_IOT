6ba9a7cd83aed7608368e2671d2090c8833aa31a
fadhilelrizanda <fadhilelrizanda@gmail.com>
2023-07-01 22:55:45 +0700

    testing dep7

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-const routes = require("./routes/routes");
-const chicken = require("./routes/chicken");
+// index.js
 const express = require("express");
-const mongoose = require("mongoose");
-// const mongoString = process.env.DATABASE_URL;
-const mongoString =
-  "mongodb+srv://fadhilelrizandamicr:xU4xokwD6X2Xi6Gn@cluster0.h4v4j79.mongodb.net/";
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-
-database.on("error", (error) => {
-  console.log(error);
-});
-
-database.once("connected", () => {
-  console.log("Database Connected");
-});
 const app = express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("Hey this is my API running 🥳");
 });
 
-app.use(express.json());
-// const routes = require("./rout es/routes");
-app.use("/chicken", chicken);
-app.use("/api", routes);
-app.listen(5000, () => {
-  console.log(`Server Started at ${5000}`);
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
 });
+
+// Export the Express API
+module.exports = app;
